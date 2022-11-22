337575d21358eee5667f6fcd47289c99f1f27fc0
Lemii <25596204+Lemii@users.noreply.github.com>
2022-11-22 08:15:05 +0100

    Refactor Selector (#9)
    * add possibility to use custom components
    
    * make color and position props optional
    
    * use Vector3 from R3F

--- src/components/Selector.tsx
+++ src/components/Selector.tsx
@@ -1,8 +1,8 @@
 import { animated, useSpring } from '@react-spring/three';
 import { Float, Shadow, Sparkles, Sphere as SphereDrei } from '@react-three/drei';
+import { Vector3 } from '@react-three/fiber';
 import { Interactive, XRInteractionEvent } from '@react-three/xr';
 import { ReactElement, useState } from 'react';
-import { Vector3 } from 'three';
 
 import { isInRange } from '../utils';
 
@@ -10,12 +10,19 @@ const selectorDistance = 10;
 
 type SelectorProps = {
   onSelect: () => void;
-  color: string;
-  position: Vector3;
+  color?: string;
+  position?: Vector3;
   sparklesCount?: number;
+  customComponent?: ({ color }: GlobeProps) => JSX.Element;
 };
 
-export const Selector = ({ onSelect, color, position, sparklesCount = 50 }: SelectorProps): ReactElement => {
+export const Selector = ({
+  onSelect,
+  color = 'white',
+  position = [0, 0, 0],
+  sparklesCount = 50,
+  customComponent,
+}: SelectorProps): ReactElement => {
   const [hovered, setHovered] = useState(false);
   const { scale } = useSpring({ scale: hovered ? 1.2 : 1 });
 
@@ -31,32 +38,29 @@ export const Selector = ({ onSelect, color, position, sparklesCount = 50 }: Sele
     isInRange(event, selectorDistance) && onSelect();
   };
 
+  const Component = customComponent || Globe;
+
   return (
     <animated.group position={position} scale={scale}>
       <Float>
         <Interactive onHover={handleHover} onBlur={handleBlur} onSelect={handleOnSelect}>
-          <Globe color={color} sparklesCount={sparklesCount} />
+          <Component color={color} />
+          <Sparkles position={[0, 1.1, 0]} size={1.5} scale={1} color={color} count={sparklesCount} />
         </Interactive>
       </Float>
-
       <Shadow />
     </animated.group>
   );
 };
 
 type GlobeProps = {
-  color: string;
-  sparklesCount: number;
+  color?: string;
 };
 
-const Globe = ({ color, sparklesCount }: GlobeProps): ReactElement => {
+const Globe = ({ color }: GlobeProps) => {
   return (
-    <>
-      <SphereDrei scale={0.5} position={[0, 1, 0]} args={[1, 10, 10]}>
-        <meshStandardMaterial transparent opacity={0.4} wireframe color={color} />
-      </SphereDrei>
-
-      <Sparkles position={[0, 1.1, 0]} size={1.5} scale={1} color={color} count={sparklesCount} />
-    </>
+    <SphereDrei scale={0.5} position={[0, 1, 0]} args={[1, 10, 10]}>
+      <meshStandardMaterial transparent opacity={0.4} wireframe color={color} />
+    </SphereDrei>
   );
 };
